542d7bef04f047c439c315606c70b253a355efd1
Steven Anderson <steven.anderson@nonlinear.com>
2016-03-09 09:52:56 +0000

    Bit of refactoring

--- network.fs
+++ network.fs
@@ -7,6 +7,8 @@ module Network =
 
   let sigmoid z = 1.0 / (1.0 + exp -z)
   let sigmoid' z = sigmoid(z) * (1.0 - sigmoid(z))
+  let feedForward w a b = w * a + b
+  let backPropErrors delta zs = delta.* (Matrix.map sigmoid' zs)
 
   type LayerConfig =
     { weights: Matrix<float>; biases: Vector<float> }
@@ -40,10 +42,10 @@ module Network =
     let randomLayers =
       let skipLast = Seq.take (numLayers - 1) layerSizes
       let skipFirst = Seq.skip 1 layerSizes
-      Seq.zip skipLast skipFirst |> Seq.map (fun x -> LayerConfig.rand(x)) |> Seq.toList
+      Seq.zip skipLast skipFirst |> Seq.map LayerConfig.rand |> Seq.toList
 
     let feedForwardLayer batchSize (activations: Activations list) (thisLayer: LayerConfig)  =
-        let zs = thisLayer.weights * (List.head activations).activations + thisLayer.batchBiases(batchSize)
+        let zs = feedForward thisLayer.weights (List.head activations).activations (thisLayer.batchBiases(batchSize))
         Activations.withWeightedInputs(zs) :: activations
 
     let feedForwardBatch (layers: LayerConfig list) inputMatrix =
@@ -51,26 +53,21 @@ module Network =
       List.fold f [Activations.inputLayer(inputMatrix)] layers
 
     let backPropLayer (errors: Matrix<float> list) (nextLayer: LayerConfig, thisActivations: Activations) =
-      let nextErrors = List.head errors
-      let thisZs = thisActivations.weightedInputs
-      (nextLayer.weights.Transpose() * nextErrors).* (Matrix.map sigmoid' thisZs) :: errors
+      backPropErrors (nextLayer.weights.Transpose() * List.head errors) thisActivations.weightedInputs :: errors
 
-    let updateLayer learningRate batchSize (oldLayer : LayerConfig, biasErrors : Matrix<float>, weightErrors: Matrix<float>) =
-      {
-        biases = oldLayer.biases - biasErrors.RowSums().Multiply(learningRate/ float batchSize)
-        weights = oldLayer.weights - weightErrors.Multiply(learningRate/float batchSize)
-      }
+    let updateLayer factor (oldLayer : LayerConfig, biasErrors : Matrix<float>, weightErrors: Matrix<float>) =
+      { biases = oldLayer.biases - biasErrors.RowSums().Multiply(factor)
+        weights = oldLayer.weights - weightErrors.Multiply(factor) }
 
     member this.evaluate(inputMatrix) =
       feedForwardBatch randomLayers inputMatrix
 
     member this.gradientDescent(train, test, epochs, batchSize, learningRate) =
 
-      let runBatch layers (batch : Batch, i) =
+      let runBatch makeLayer layers (batch : Batch) =
         let activations = feedForwardBatch layers batch.inputMatrix
         let outputActivations = List.head activations
-        let costDerivatives = outputActivations.activations - batch.expectedMatrix
-        let layerErrors = [costDerivatives.* (Matrix.map sigmoid' outputActivations.weightedInputs)]
+        let layerErrors = [backPropErrors (outputActivations.activations - batch.expectedMatrix) outputActivations.weightedInputs]
 
         let allButLastActivations = activations |> List.tail |> List.rev
         let middleActivations = allButLastActivations |> List.tail
@@ -80,7 +77,7 @@ module Network =
         let calcWeightError (prevActivations, thisErrors) = thisErrors * prevActivations.activations.Transpose()
         let weightErrors = Seq.map calcWeightError (Seq.zip allButLastActivations biasErrors)
 
-        Seq.zip3 layers biasErrors weightErrors |> Seq.map (updateLayer learningRate batchSize) |> Seq.toList
+        Seq.zip3 layers biasErrors weightErrors |> Seq.map makeLayer |> Seq.toList
 
       let runEpoch initialLayers i =
         printfn "Epoch %i" i
@@ -97,7 +94,7 @@ module Network =
         printfn "%i batches" batches.Length
 
         // TODO: Evaluate after each epoch
-        let newLayers = Seq.fold runBatch initialLayers (Seq.mapi (fun i x -> x,i) batches) |> Seq.toList
+        let newLayers = Seq.fold (runBatch (updateLayer (learningRate / float batchSize))) initialLayers batches |> Seq.toList
         printfn "Took %A" sw.Elapsed
         newLayers
 
